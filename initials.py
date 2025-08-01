0d0bd4f6030da07113713dcf610ed60d3ac5901a
horstandr <andras.schiphorst@icloud.com>
2025-08-01 18:22:32 +0200

    commit

--- initials.py
+++ initials.py
@@ -1,19 +1,28 @@
 from PIL import Image, ImageDraw, ImageFont
 import random
 
-poster = Image.open('background_images/1.png').convert('RGBA').resize((1080,1080))
+capital_letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-# DRAW TITLE
-draw = ImageDraw.Draw(poster)
-font = ImageFont.truetype('fonts/tiktoksans/static/TikTokSans_18pt_Condensed-Regular.ttf', 40)
-draw.text((60,60), "These initials go extremely well together:", font=font, fill="white", stroke_fill="black", stroke_width=2)
+def initials_img(width=1080, height=1080, tile_width=3, tile_height=5, title=""):
+    # init
+    poster = Image.open(f'background_images/{random.randint(1,5)}.png').convert('RGBA').resize((width,height))
 
-width = 2
-height = 5
+    # DRAW TITLE
+    draw = ImageDraw.Draw(poster)
+    font = ImageFont.truetype('fonts/tiktoksans/static/TikTokSans_18pt_Condensed-Regular.ttf', 40)
+    draw.text((60,60), "These initials go extremely well together:", font=font, fill="white", stroke_fill="black", stroke_width=2)
 
-# DRAW INITIALS
-for i in range(height):
-    for j in range(width):
-        draw.text((60+(j*120), 120+(i*60)), "W+W", font=font, fill="white", align="left", stroke_fill="black", stroke_width=2)
+    # DRAW INITIALS
 
-poster.save("output/output_poster.png")
+    for i in range(tile_height):
+        for j in range(tile_width):
+            draw.text((60+(j*120), 120+(i*60)), 
+                    f"{capital_letters[random.randint(0,25)]}+{capital_letters[random.randint(0,25)]}", 
+                    font=font, 
+                    fill="white", 
+                    align="left", 
+                    stroke_fill="black", stroke_width=2)
+
+    poster.save("output/output_poster.png")
+
+initials_img()
